7a813207e1795040afad8a6f7284c84586e64c7c
lgw <lgwny0120@163.com>
2022-08-14 21:58:59 +0800

    通过os对文件进性操着

--- 13.os模块.py
+++ 13.os模块.py
@@ -2,14 +2,14 @@
 # os中的函数:
 import os
 # ---------------------查看当前文件所在文件夹-----------------
-file_all = os.getcwd()
-print(file_all)
+# file_all = os.getcwd()
+# print(file_all)
 # ---------------------查看当前文件所在文件夹-----------------
-file_all = os.path.dirname(__file__)
-print(file_all)
+# file_all = os.path.dirname(__file__)
+# print(file_all)
 # ---------------------查看当前文件夹所有文件(所有同级目录)-----------------
-file_list = os.listdir(file_all)
-print(file_list)
+# file_list = os.listdir(file_all)
+# print(file_list)
 # ---------------------创建文件夹-----------------
 # if not os.path.exists(r'D:\Python\Git-python\p'):
 #     f = os.mkdir(r'D:\Python\Git-python\p')  # (要创建的文件夹地址)
@@ -31,17 +31,145 @@ print(file_list)
 #
 # print('删除成功')
 # ---------------------切换文件夹-----------------
-path = os.getcwd()
-print(path)
-f = os.chdir(r'D:\Python\pycham')
-print(f)
-path = os.getcwd()
-print(path)
-file_list = os.listdir(path)
-print(file_list)
-# ---------------------查看当前文件所在文件夹-----------------
-# ---------------------查看当前文件所在文件夹-----------------
-# ---------------------查看当前文件所在文件夹-----------------
+# path = os.getcwd()
+# print(path)
+# f = os.chdir(r'D:\Python\pycham')  # 切换文件夹
+# print(f)
+# path = os.getcwd()  # 查看是否切换成功
+# print(path)
+# file_list = os.listdir(path)  # 查看切换后文件夹目录
+# print(file_list)
+# ---------------------复制文件夹(只能复制单层文件夹，文件夹中套文件夹则不行， 需优化)-----------------
+# src_path = r'files'
+# target_path = r'files2'
+# 封装成函数
 
+
+# def copy_func(src, target):
+#     if os.path.isdir(src) and os.path.isdir(target):
+#         fileList = os.listdir(src)  # 将文件以列表形式返回
+#         print(fileList)
+#         for file in fileList:
+#             path = os.path.join(src, file)
+#             print(path)
+#             with open(path, 'rb') as stream_in:
+#                 container = stream_in.read()
+#
+#                 path1 = os.path.join(target,file)
+#                 with open(path1, 'wb') as stream_out:
+#                     stream_out.write(container)
+#
+#     else:
+#         print('复制完成')
+#
+#
+# copy_func(src_path, target_path)
+# ---------------------复制文件夹(可解决文件夹中套文件夹，但新的文件夹中的内容会统一放到一起，不会像原目录， 需优化)
+# src_path = r'files'
+# target_path = r'files2'
+# # 封装成函数
+#
+#
+# def copy(src_path, target_path):
+#     # 获取文件夹的内容
+#     fileList = os.listdir(src_path)
+#     # 变量列表
+#     print(fileList)
+#     for file in fileList:
+#         # 拼接路径
+#         path = os.path.join(src_path, file)
+#         print(path)
+#         # 判断是文件夹还是文件
+#         if os.path.isdir(path):
+#             # os.chdir(path)
+#             # 调用函数(递归)
+#             copy(path, target_path)
+#         else:
+#             # 是文件则直接进行复制
+#             with open(path, 'rb') as stream_in:
+#                 container = stream_in.read()
+#
+#                 path1 = os.path.join(target_path, file)
+#                 with open(path1, 'wb') as stream_out:
+#                     stream_out.write(container)
+#     else:
+#         print('复制完成')
+#
+#
+# copy(src_path, target_path)
+# ---------------------查看当前文件所在文件夹-----------------
+# src_path = r'files'
+# target_path = r'files2'
+# # 封装成函数
+#
+#
+# def copy(src_path, target_path):
+#     # 获取文件夹的内容
+#     fileList = os.listdir(src_path)
+#     # 变量列表
+#     print(fileList)
+#     for file in fileList:
+#         # 拼接路径
+#         path = os.path.join(src_path, file)
+#         print(path)
+#         # 判断是文件夹还是文件
+#         if os.path.isdir(path):
+#             # os.chdir(path)
+#             # 调用函数(递归)
+#             target_path1=os.path.join(target_path, file)
+#             os.mkdir(target_path1)
+#             copy(path, target_path1)
+#         else:
+#             # 是文件则直接进行复制
+#             with open(path, 'rb') as stream_in:
+#                 container = stream_in.read()
+#
+#                 path1 = os.path.join(target_path, file)
+#                 with open(path1, 'wb') as stream_out:
+#                     stream_out.write(container)
+#     else:
+#         print('复制完成')
+#
+#
+# copy(src_path, target_path)
+# --------------------------------------------------------------
+src_path = r'files'
+target_path = r'files2'
+# 封装成函数
+
+
+def copy(src_path, target_path):
+    # 获取文件夹的内容
+    fileList = os.listdir(src_path)
+    # 变量列表
+    print(fileList)
+    for file in fileList:
+        # 拼接路径
+        path = os.path.join(src_path, file)
+        print(path)
+        # 判断是文件夹还是文件
+        if os.path.isdir(path):
+            # os.chdir(path)
+            # 调用函数(递归)
+            # 这里的路径是要改成目标路径
+            path2 = os.path.join(target_path, file)
+            # 在目标路径创建文件夹
+            os.mkdir(path2)
+            # 调用递归函数
+            copy(path, path2)
+
+        else:
+            # 是文件则直接进行复制
+            with open(path, 'rb') as stream_in:
+                container = stream_in.read()
+
+                path1 = os.path.join(target_path, file)
+                with open(path1, 'wb') as stream_out:
+                    stream_out.write(container)
+    else:
+        print('复制完成')
+
+
+copy(src_path, target_path)
 
 
---
+++ num.py
@@ -0,0 +1,4 @@
+print(3+6)
+print(3*6)
+print(3-1)
+print(6/2)
