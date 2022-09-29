d52cc0884950867b13462d445e93a9e1ca92c564
lgw <lgwny0120@163.com>
2022-09-29 20:56:32 +0800

    对象的简单应用

--- 19.object.py
+++ 19.object.py
@@ -80,3 +80,77 @@ print(mt.name)
 Student.name = 'SGC'
 print(lgw.name)
 
+# ------近几次课的总结--------
+# 列表推导式 [表达式 for 变量 in 列表]
+list1 = [1, 2, 3]  # ----> 列表
+y = [x + 2 for x in list1]  # ----> 产生新的列表
+print(y)
+# 1.[表达式 for 变量 in 列表 if 条件]
+y1 = [x + 2 for x in list1 if x % 2 == 0]
+print(y1)
+# 2.[结果A if 条件 else 结果B for 变量 in 列表 ]
+y2 = [x + 2 if x % 2 == 0 else x + 1 for x in list1]
+print(y2)
+
+
+# -------y3解释y2------
+
+
+def func():
+    list_new = []
+    for i in list1:
+        if i % 2 == 0:
+            i += 2
+            list_new.append(i)
+        else:
+            i += 1
+            list_new.append(i)
+    return list_new
+
+
+y3 = func()
+print(y3)
+
+# ------------------------------------
+# 类中方法: 动作
+# 种类: 普通方法 类方法 静态方法 魔术方法
+# -------------------
+'''
+普通方法:
+def 方法名(self,[,参数，参数]):
+    pass
+'''
+
+
+class Phone1:
+    brand = 'xiaomi'
+    price = 4999
+    type = 'Ultra'
+
+    # Phone类里面方法名: call
+    def call(self):
+        print('self-------,', self)
+        print('正在打电话...')
+        print('这是我的note:', self.note)
+        print('正在访问通讯录:')
+        for person in self.address_list:
+            print(person.items())
+        print('正在打电话...')
+        print('留言:',self.note)
+
+
+phone2 = Phone1()
+phone2.note = 'phone2'
+phone2.address_list = [{'lgw': '15890716205'},
+                       {'lx': '12345678'},
+                       {'mcl': '8888888'}]
+print(phone2, 'self-------1')
+phone2.call()
+
+phone3 = Phone1()
+phone3.note = 'phone3'
+phone3.address_list = [{'lgw': '15890716205'},
+                       {'lx': '12345678'},
+                       {'mcl': '8888888'}]
+print(phone3, 'self-------2')
+phone3.call()
